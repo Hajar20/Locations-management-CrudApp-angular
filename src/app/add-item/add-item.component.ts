5f4eb3279a58005fdb28e2bcdc591c3c8a7a0f71
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2023-11-17 17:07:58 +0100

    add_v1

--- src/app/add-item/add-item.component.ts
+++ src/app/add-item/add-item.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AppService} from "../app.service";
 
 @Component({
   selector: 'app-add-item',
   templateUrl: './add-item.component.html',
   styleUrls: ['./add-item.component.css']
 })
-export class AddItemComponent {
+export class AddItemComponent implements OnInit{
+ constructor(private app : AppService) {
+ }
+   stateCodes:any[] = [];
+ ngOnInit() {
+   this.stateCodes = [ "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
+     "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+     "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+     "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+     "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+ }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ListItemComponent } from './list-item/list-item.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import {HttpClientModule} from "@angular/common/http";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
